5357142f30c87cd89b912b0a43739aab1bd6c619
rashad <omar_rashad@aol.com>
2020-12-22 18:50:54 +0200

    fixing printing the board logic

--- lib/board.rb
+++ lib/board.rb
@@ -18,17 +18,17 @@ class Board
     current_board = []
     (0..12).each do |index|
       if [0, 1, 2, 3, 4, 5, 6, 7, 8].include? index
-        current_board.unshift("  #{index + 1}  | [#{code_breaker_board[index][0]}][#{code_breaker_board[index][1]}]" /
+        current_board.unshift("  #{index + 1}  | [#{code_breaker_board[index][0]}][#{code_breaker_board[index][1]}]" \
         "[#{code_breaker_board[index][2]}][#{code_breaker_board[index][3]}] [#{pins_board[index].join}] |\n")
       end
       if [10, 11].include? index
-        current_board.unshift("  #{index + 1} | [#{code_breaker_board[index][0]}][#{code_breaker_board[index][1]}]" /
+        current_board.unshift("  #{index + 1} | [#{code_breaker_board[index][0]}][#{code_breaker_board[index][1]}]" \
         "[#{code_breaker_board[index][2]}][#{code_breaker_board[index][3]}] [#{pins_board[index].join}] |\n")
       end
       next unless index == 12
 
-      current_board.unshift("     | [#{code_maker_board[0].gsub(/[rgby]/, '#')}]" /
-      "[#{code_maker_board[1].gsub(/[rgby]/, '#')}][#{code_maker_board[2].gsub(/[rgby]/, '#')}]" /
+      current_board.unshift("     | [#{code_maker_board[0].gsub(/[rgby]/, '#')}]" \
+      "[#{code_maker_board[1].gsub(/[rgby]/, '#')}][#{code_maker_board[2].gsub(/[rgby]/, '#')}]" \
       "[#{code_maker_board[3].gsub(/[rgby]/, '#')}]        |\n")
     end
     current_board
